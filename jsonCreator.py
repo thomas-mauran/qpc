9e5c3d37d3b8ece2561ccdec8e99112754031733
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2022-10-16 19:52:34 +0200

    Ajout dans la db grace au script python

--- jsonCreator.py
+++ jsonCreator.py
@@ -1,20 +1,30 @@
 import json
+import requests
+import time
+
 
 file = open('qpc.txt', 'r')
 jsonFile = open('questionJson.json', 'w')
+url = 'http://localhost:7512/qpc/questions/_create'
 
 lines = file.readlines()
 
+i = 0
 for l in lines: 
     text = l.split(':')
+
+    
     jsonLine = {
         "question": text[0],
         "answer": text[1]
     }
-    jsonFile.write(json.dumps(jsonLine))
+    # jsonFile.write(json.dumps(jsonLine))
+    response = requests.post(url, jsonLine)
+    print(response)
 
-jsonFileRead = open('questionJson.json', 'r')
 
-lines2 = jsonFileRead.readlines()
-for line in lines2:
-    print(line)
+# jsonFileRead = open('questionJson.json', 'r')
+
+# lines2 = jsonFileRead.readlines()
+# for line in lines2:
+#     print((line))
