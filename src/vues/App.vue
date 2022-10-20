f12dd24cf8f9b1046148a66b4edc5ba2e0d99dc4
Thomas Mauran <78204354+thomas-mauran@users.noreply.github.com>
2022-10-20 21:59:49 +0200

    Check answer fonctionne

--- src/vues/App.vue
+++ src/vues/App.vue
@@ -8,13 +8,15 @@ let inputAnswer = ref('');
 
 let question = ref('');
 let answer = ref('');
+let answerTab = ref(null);
+
+let answerIsTrue = ref(null);
 
 async function retrieveData() {
     console.log('retrieving data');
     await kuzzle.connect();
     let numberOfQuestions = await kuzzle.document.count('qpc', 'questions');
     let randomInt = Math.floor(Math.random() * (numberOfQuestions - 1));
-    console.log(randomInt);
     let questionQuery = await kuzzle.document.search('qpc', 'questions', {
         query: {
             match: {
@@ -25,32 +27,91 @@ async function retrieveData() {
 
     question.value = questionQuery.hits[0]._source.question;
     answer.value = questionQuery.hits[0]._source.answer;
+    answerTab.value = answer.value.split('/');
 }
 
+// Fonction pour comparer l'input et la liste de réponses
+
 function checkAnswer() {
+    // On enlève les maj et les espaces
+    let convertedString = inputAnswer.value.toLocaleLowerCase();
+    convertedString = convertedString.replaceAll(' ', '');
+
+    answerTab.value.forEach((a) => {
+        // On enlève les maj et les espaces + on remplace les appostrophes
+
+        let tempA = a.toLocaleLowerCase();
+        tempA = tempA.replaceAll(' ', '');
+        tempA = tempA.replaceAll('’', "'");
+
+        // Si les 2 valeures sont égales answerIsTrue devient true
+        if (tempA == convertedString) {
+            answerIsTrue.value = true;
+            console.log('singe');
+            return;
+        }
+    });
+    // answerIsTrue est toujours nulle la réponse est donc fausse
+    if (answerIsTrue.value == null) answerIsTrue.value = false;
+
     return;
 }
 
+// Au load de la page on recup les datas
 onMounted(() => {
     retrieveData();
+});
+
+function nextQuestion() {
+    question.value = '';
+    answer.value = '';
+    answerIsTrue.value = null;
+    inputAnswer.value = '';
+
+    retrieveData();
+}
+
+document.addEventListener('keydown', (key) => {
+    // if(answerIsTrue.value == null){
+    //     checkAnswer()
+    // }
+    if (key.code == 'Enter') {
+        answerIsTrue.value == null ? checkAnswer() : nextQuestion();
+    }
 });
 </script>
 
 <template>
     <div>
         <h1>{{ question }}</h1>
-        <input
-            class="input"
-            placeholder="Ta réponse"
-            type="text"
-            v-model="inputAnswer"
-        />
-        <button @click="checkAnswer">Check</button>
+        <h2 v-if="answerIsTrue" class="rightAnswer">Correct answer</h2>
+        <h2 v-if="answerIsTrue == false" class="wrongAnswer">Wrong answer</h2>
+
+        <div v-if="answerIsTrue != null">
+            <h2>Answer : {{ answer }}</h2>
+            <button @click="nextQuestion">Next question</button>
+        </div>
+        <div v-if="answerIsTrue == null">
+            <input
+                class="input"
+                placeholder="Ta réponse"
+                type="text"
+                v-model="inputAnswer"
+            />
+            <button @click="checkAnswer">Check</button>
+        </div>
     </div>
 </template>
 
 <style scoped>
 .input {
     font-size: 2em;
+}
+
+.rightAnswer {
+    color: green;
+}
+.wrongAnswer {
+    color: red;
 }
 </style>
